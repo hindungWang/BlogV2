ddf9650e6868bf18104ab3530cbce277cd2b64bd
hxd <836778763@qq.com>
2020-08-23 23:07:29 +0800

    fix dooter

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -14,7 +14,7 @@ export default class Footer extends Component {
 									<div className="content">
 										<div className="columns is-vcentered">
 											<div className="column is-7">												
-												<p className="beian"><span className ="emoji" role ="img" aria-label="heart">✨</span>️but i am still around, yeah i guoweiam still around.</p>
+												<p className="beian"><span className ="emoji" role ="img" aria-label="heart">✨</span>️But i am still around, yeah i am still around.</p>
 												<p className="beian"><span className ="emoji" role ="img" aria-label="heart">🤩</span>️路漫漫其修远兮，吾将上下而求索。</p>
 												<p className="beian">{`CopyRight. 2020-${new Date().getFullYear()} 显东. All rights reserved.`}</p>
 												<p className="beian"><span className ="emoji" role ="img" aria-label="heart"></span><a className="beian" href="http://www.beian.miit.gov.cn">桂ICP备20006258号</a>	</p>
