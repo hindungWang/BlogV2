727ea5219b19557b042e659bd8752c943d52b881
xiandonghuang <xiandonghuang@caih.com>
2021-06-03 10:34:48 +0800

    修改标题顺序

--- src/BlogDetailContainer/BlogDetailContainer.js
+++ src/BlogDetailContainer/BlogDetailContainer.js
@@ -58,10 +58,10 @@ export default class BlogDetailContainer extends Component {
         let arr = Array.from(document.querySelectorAll('h1,h2,h3,h4,h5,h6'));
         let htmlArr = arr.map((item) => {
             let newItem;
-            item.localName === 'h1' ? newItem = `<li><a title='${item.innerText}' href='#${item.id}'>😆${item.innerText}</a></li>` : null;
-            item.localName === 'h2' ? newItem = `<ul><li><a title='${item.innerText}' href='#${item.id}'>😆${item.innerText}</a></li></ul>` : null;
-            item.localName === 'h3' ? newItem = `<ul><ul><li><a title='${item.innerText}' href='#${item.id}'>🤣${item.innerText}</a></li></ul></ul>` : null;
-            item.localName === 'h4' ? newItem = `<ul><ul><ul><li><a title='${item.innerText}' href='#${item.id}'>😉${item.innerText}</a></li></ul></ul></ul>` : null;
+            item.localName === 'h1' ? newItem = `<li><a title='${item.innerText}' href='#${item.id}'>${item.innerText}</a></li>` : null;
+            item.localName === 'h2' ? newItem = `<ul><li><a title='${item.innerText}' href='#${item.id}'>&nbsp;&nbsp;&nbsp;${item.innerText}</a></li></ul>` : null;
+            item.localName === 'h3' ? newItem = `<ul><ul><li><a title='${item.innerText}' href='#${item.id}'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${item.innerText}</a></li></ul></ul>` : null;
+            item.localName === 'h4' ? newItem = `<ul><ul><ul><li><a title='${item.innerText}' href='#${item.id}'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${item.innerText}</a></li></ul></ul></ul>` : null;
             return newItem;
         });
         htmlArr.shift();
